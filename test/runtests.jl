c7ce6691da0f1236044648984149d9268debff7f
Michael Eastwood <mweastwood@astro.caltech.edu>
2015-04-15 09:44:11 -0700

    Minor test addition

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,8 @@ using HEALPix
 using Base.Test
 
 for nside = 1:5
-    @test nside2npix(nside) == 12*nside^2
+    npix = 12*nside^2
+    @test nside2npix(nside) == npix
+    @test npix2nside(npix) == nside
 end
 
