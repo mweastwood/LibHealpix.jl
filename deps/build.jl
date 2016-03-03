15984f5fd6ef1c260f52f1ea1bc4adaf7da30fdf
Michael Eastwood <mweastwood@astro.caltech.edu>
2016-03-03 12:44:53 -0800

    start work on trying to make the build system more robust

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,27 @@
+using BinDeps
+@BinDeps.setup
+
+chealpix       = library_dependency("libchealpix",       runtime = true)
+healpix_cxx    = library_dependency("libhealpix_cxx",    runtime = false)
+healpixwrapper = library_dependency("libhealpixwrapper", runtime = true, depends = [healpix_cxx])
+
+@osx_only begin
+    if Pkg.installed("Homebrew") === nothing
+        error("Homebrew package not installed, please run Pkg.add(\"Homebrew\")")
+    end
+    using Homebrew
+    #provides(Homebrew.HB, "cfitsio", cfitsio, os = :Darwin)
+end
+
+provides(AptGet, Dict("libchealpix-dev" => chealpix,
+                      "libhealpix-cxx-dev" => healpix_cxx))
+
+# Linux build from source
+
+@BinDeps.install Dict(:chealpix => :libchealpix, :healpixwrapper => :libhealpixwrapper)
+
+
+#=
 depsdir = dirname(@__FILE__)
 
 # Download the HEALPix source
@@ -17,4 +41,5 @@ println("Building the HEALPix wrapper...")
 dir = joinpath(depsdir,"src")
 run(`make -C $dir`)
 run(`make -C $dir install`)
+=#
 
