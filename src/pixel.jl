25b3bbd7d7cf9d018a884a8aa6206d77898e70b5
Michael Eastwood <michael.w.eastwood@gmail.com>
2016-03-06 22:46:37 -0800

    minor clean up to pixel.jl
    * don't redefine functions if Clong and Clonglong are the same type
    * use Ref when possible
    * add a whole bunch of spaces everywhere to make things more readable

--- src/pixel.jl
+++ src/pixel.jl
@@ -16,74 +16,75 @@
 ################################################################################
 # Pixel operations
 
-for T in (:Clong,:Clonglong)
+types = (Clong == Clonglong)? (:Clong,) : (:Clong, :Clonglong)
+for T in types
     # Append "64" to the ccall function name when defining for Clonglong
     # (note we omit this suffix from the Julia function name -- just let dispatch take care of it)
-    funcname(f) = (T==:Clong)? string(f) : string(f)*"64"
+    funcname(f) = (T == :Clong)? string(f) : string(f)*"64"
 
-    for f in (:ang2pix_nest,:ang2pix_ring)
-        @eval function $f(nside::$T,θ::Cdouble,ϕ::Cdouble)
-            ipixptr = Array($T,1)
-            0 <= θ <= π || error("θ is out of range")
-            ccall(($(funcname(f)),libchealpix),Void,($T,Cdouble,Cdouble,Ptr{$T}),nside,θ,ϕ,ipixptr)
-            ipixptr[1] + 1 # Add one to convert to a 1-indexed scheme
+    for f in (:ang2pix_nest, :ang2pix_ring)
+        @eval function $f(nside::$T, θ::Cdouble, ϕ::Cdouble)
+            ipixptr = Ref{$T}(0)
+            0 ≤ θ ≤ π || error("θ is out of range")
+            ccall(($(funcname(f)), libchealpix), Void, ($T, Cdouble, Cdouble, Ref{$T}), nside, θ, ϕ, ipixptr)
+            ipixptr[] + 1 # Add one to convert to a 1-indexed scheme
         end
     end
 
-    for f in (:pix2ang_nest,:pix2ang_ring)
-        @eval function $f(nside::$T,ipix::$T)
+    for f in (:pix2ang_nest, :pix2ang_ring)
+        @eval function $f(nside::$T, ipix::$T)
             ipix -= 1 # Subtract one to convert back to a 0-indexed scheme
-            θptr = Array(Cdouble,1)
-            ϕptr = Array(Cdouble,1)
-            ccall(($(funcname(f)),libchealpix),Void,($T,$T,Ptr{Cdouble},Ptr{Cdouble}),nside,ipix,θptr,ϕptr)
-            θptr[1],ϕptr[1]
+            θptr = Ref{Cdouble}(0.0)
+            ϕptr = Ref{Cdouble}(0.0)
+            ccall(($(funcname(f)), libchealpix), Void, ($T, $T, Ref{Cdouble}, Ref{Cdouble}), nside, ipix, θptr, ϕptr)
+            θptr[], ϕptr[]
         end
     end
 
-    for f in (:nest2ring,:ring2nest)
-        @eval function $f(nside::$T,ipix::$T)
+    for f in (:nest2ring, :ring2nest)
+        @eval function $f(nside::$T, ipix::$T)
             ipix -= 1 # Subtract one to convert back to a 0-indexed scheme
-            ipixoutptr = Array($T,1)
-            ccall(($(funcname(f)),libchealpix),Void,($T,$T,Ptr{$T}),nside,ipix,ipixoutptr)
-            ipixoutptr[1] + 1 # Add one to convert to a 1-indexed scheme
+            ipixoutptr = Ref{$T}(0)
+            ccall(($(funcname(f)), libchealpix), Void, ($T, $T, Ref{$T}), nside, ipix, ipixoutptr)
+            ipixoutptr[] + 1 # Add one to convert to a 1-indexed scheme
         end
     end
 
-    for f in (:nside2npix,:npix2nside)
-        @eval $f(x::$T) = ccall(($(funcname(f)),libchealpix),$T,($T,),x)
+    for f in (:nside2npix, :npix2nside)
+        @eval $f(x::$T) = ccall(($(funcname(f)), libchealpix), $T, ($T,), x)
     end
 
-    for f in (:vec2pix_nest,:vec2pix_ring)
-        @eval function $f(nside::$T,vec::Vector{Cdouble})
-            ipixptr = Array($T,1)
-            ccall(($(funcname(f)),libchealpix),Void,($T,Ptr{Cdouble},Ptr{$T}),nside,vec,ipixptr)
-            ipixptr[1] + 1 # Add one to convert to a 1-indexed scheme
+    for f in (:vec2pix_nest, :vec2pix_ring)
+        @eval function $f(nside::$T, vec::Vector{Cdouble})
+            ipixptr = Ref{$T}(0)
+            ccall(($(funcname(f)), libchealpix), Void, ($T, Ptr{Cdouble}, Ref{$T}), nside, vec, ipixptr)
+            ipixptr[] + 1 # Add one to convert to a 1-indexed scheme
         end
     end
 
-    for f in (:pix2vec_nest,:pix2vec_ring)
-        @eval function $f(nside::$T,ipix::$T)
+    for f in (:pix2vec_nest, :pix2vec_ring)
+        @eval function $f(nside::$T, ipix::$T)
             ipix -= 1 # Subtract one to convert back to a 0-indexed scheme
             vec = Array(Cdouble,3)
-            ccall(($(funcname(f)),libchealpix),Void,($T,$T,Ptr{Cdouble}),nside,ipix,vec)
+            ccall(($(funcname(f)), libchealpix), Void, ($T, $T, Ptr{Cdouble}), nside, ipix, vec)
             vec
         end
     end
 end
 
-function ang2vec(θ::Cdouble,ϕ::Cdouble)
+function ang2vec(θ::Cdouble, ϕ::Cdouble)
     vec = Array(Cdouble,3)
-    ccall(("ang2vec",libchealpix),Void,(Cdouble,Cdouble,Ptr{Cdouble}),θ,ϕ,vec)
+    ccall(("ang2vec", libchealpix), Void, (Cdouble, Cdouble, Ptr{Cdouble}), θ, ϕ, vec)
     vec
 end
 
 function vec2ang(vec::Vector{Cdouble})
-    θptr = Array(Cdouble,1)
-    ϕptr = Array(Cdouble,1)
-    ccall(("vec2ang",libchealpix),Void,(Ptr{Cdouble},Ptr{Cdouble},Ptr{Cdouble}),vec,θptr,ϕptr)
-    θptr[1],ϕptr[1]
+    θptr = Ref{Cdouble}(0.0)
+    ϕptr = Ref{Cdouble}(0.0)
+    ccall(("vec2ang", libchealpix), Void, (Ptr{Cdouble}, Ref{Cdouble}, Ref{Cdouble}), vec, θptr, ϕptr)
+    θptr[], ϕptr[]
 end
 
-npix2nside(npix) = npix2nside(Clong(npix))
-nside2npix(nside) = nside2npix(Clong(nside))
+npix2nside(npix) = npix2nside(Int(npix))
+nside2npix(nside) = nside2npix(Int(nside))
 
